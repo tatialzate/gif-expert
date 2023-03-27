49a755ac80387977aabd7acef66e21650f5df36a
Wendy Tatiana Alzate Cardona <walzate@WALZATEMAC.local>
2023-03-27 17:35:56 -0500

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { AddCategory, GifGrid} from './components';
+
+export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState(['One Punch']);
+
+    const onAddCategory = ( newCategory ) => {
+
+        if( categories.includes( newCategory) ) return;
+        setCategories([newCategory, ...categories]);
+    }
+
+    return (
+        <React.Fragment>
+            <h1>GifExpertApp</h1>
+            <AddCategory onNewCategory= { onAddCategory } />
+          
+            {
+                categories.map( category => {
+                    return <GifGrid key={category} category={category}/>
+                })
+            }
+        </React.Fragment>
+    )
+}
